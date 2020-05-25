06892c2f0e93dec28fa94efffb536f8d25d2bf22
BuildTools <unconfigured@null.spigotmc.org>
2020-05-25 16:51:14 +0200

    args ding

--- src/wasdenn/Main.java
+++ src/wasdenn/Main.java
@@ -96,10 +96,50 @@ public class Main extends JavaPlugin implements Listener {
             }
 
 
-        if (cmd.getName().equalsIgnoreCase("gm1"))
+        if (cmd.getName().equalsIgnoreCase("gm"))
             if (p.isOp()) {
-                p.setGameMode(GameMode.CREATIVE);
-                p.sendMessage("Set owm gamemode to Creative Mode");
+                if(args.length==1) {
+                    if(args[0].equalsIgnoreCase("0")) {
+                        p.setGameMode(GameMode.SURVIVAL);
+                        p.sendMessage("Set own gamemode to Survival Mode");
+                    }
+                    else if(args[0].equalsIgnoreCase("1")) {
+                        p.setGameMode(GameMode.CREATIVE);
+                        p.sendMessage("Set own gamemode to Creative Mode");
+                    }
+                    else if(args[0].equalsIgnoreCase("2")) {
+                        p.setGameMode(GameMode.ADVENTURE);
+                        p.sendMessage("Set own gamemode to Adventure Mode");
+                    }
+                    else if(args[0].equalsIgnoreCase("3")) {
+                        p.setGameMode(GameMode.SPECTATOR);
+                        p.sendMessage("Set own gamemode to Spectator Mode");
+                    }
+                }
+                if(args.length==2) {
+                    Player p1 = Bukkit.getPlayer(args[1]);
+                    if(!p1.isOnline()) {
+                        p.sendMessage("§cGeht nicht du huso");
+                        return true;
+                    }
+                    if(args[0].equalsIgnoreCase("0")) {
+                        p1.setGameMode(GameMode.SURVIVAL);
+                        p1.sendMessage("Set" + p1.getDisplayName() + "own gamemode to Survival Mode");
+                    }
+                    else if(args[0].equalsIgnoreCase("1")) {
+                        p1.setGameMode(GameMode.CREATIVE);
+                        p1.sendMessage("Set " + p1.getDisplayName() + "s own gamemode to Creative Mode");
+                    }
+                    else if(args[0].equalsIgnoreCase("2")) {
+                        p1.setGameMode(GameMode.ADVENTURE);
+                        p1.sendMessage("Set "  + p1.getDisplayName() + "s gamemode to Adventure Mode");
+                    }
+                    else if(args[0].equalsIgnoreCase("3")) {
+                        p1.setGameMode(GameMode.SPECTATOR);
+                        p1.sendMessage("Set " + p1.getDisplayName() + "s gamemode to Spectator Mode");
+                    }
+
+                }
                 return true;
             }
 
@@ -154,9 +194,10 @@ public class Main extends JavaPlugin implements Listener {
     public void onSend(AsyncPlayerChatEvent e) {
         Player p = e.getPlayer();
         String message = e.getMessage();
-        if(p.isOp()) e.setMessage("§3" + message);
+        if(p.isOp()) e.setMessage("§4" + message);
     }
     //ur mom has big gay
+
 }
 
 
