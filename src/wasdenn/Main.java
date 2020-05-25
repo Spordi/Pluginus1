6ed92d71d5f7b8759e9ca427f179ec4df98cfb1c
Spordi <65907355+spordi@users.noreply.github.com>
2020-05-25 18:40:10 +0200

    Neue Kommando klasse

--- src/wasdenn/Main.java
+++ src/wasdenn/Main.java
@@ -1,6 +1,5 @@
 package wasdenn;
 
-import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -11,6 +10,8 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 
+import static org.spigotmc.SpigotConfig.registerCommands;
+
 public class Main extends JavaPlugin implements Listener {
 
     ArrayList<Player> marmeladenbrotmithonig = new ArrayList<>();
@@ -19,12 +20,14 @@ public class Main extends JavaPlugin implements Listener {
     public void onEnable() {
 
         registerEvents();
+        registerCommands();
 
         this.getServer().getPluginManager().registerEvents(this, this);
 
 
         System.out.println("[Info] Plugin erfolgreich aktiviert!");
     }
+
 
     @Override
     public void onDisable() {
@@ -79,77 +82,27 @@ public class Main extends JavaPlugin implements Listener {
 
             return true;
         }
-        if (cmd.getName().equalsIgnoreCase("clear"))
-            if (p.hasPermission("Main.clear")) {
-                p.getInventory().clear();
-                p.sendMessage("Your inventory has been cleared");
-                return true;
-            }
         if (cmd.getName().equalsIgnoreCase("gm0"))
             if (p.isOp()) {
                 p.setGameMode(GameMode.SURVIVAL);
                 p.sendMessage("Set own gamemode to Survival Mode");
                 return true;
             }
-        if (cmd.getName().equalsIgnoreCase("gm"))
-            if (p.isOp()) {
-                if(args.length==1) {
-                    if(args[0].equalsIgnoreCase("0")) {
-                        p.setGameMode(GameMode.SURVIVAL);
-                        p.sendMessage("Set own gamemode to Survival Mode");
-                    }
-                    else if(args[0].equalsIgnoreCase("1")) {
-                        p.setGameMode(GameMode.CREATIVE);
-                        p.sendMessage("Set own gamemode to Creative Mode");
-                    }
-                    else if(args[0].equalsIgnoreCase("2")) {
-                        p.setGameMode(GameMode.ADVENTURE);
-                        p.sendMessage("Set own gamemode to Adventure Mode");
-                    }
-                    else if(args[0].equalsIgnoreCase("3")) {
-                        p.setGameMode(GameMode.SPECTATOR);
-                        p.sendMessage("Set own gamemode to Spectator Mode");
-                    }
-                }
-                if(args.length==2) {
-                    Player p1 = Bukkit.getPlayer(args[1]);
-                    if(p1==null) {
-                        p.sendMessage("§cGeht nicht du huso");
-                        return true;
-                    }
-                    if(args[0].equalsIgnoreCase("0")) {
-                        p1.setGameMode(GameMode.SURVIVAL);
-                        p1.sendMessage(p.getDisplayName() + " set your gamemode to Survival Mode");
-                        p.sendMessage("Set" + p1.getDisplayName() + "'s gamemode to Survival Mode");
-                    }
-                    else if(args[0].equalsIgnoreCase("1")) {
-                        p1.setGameMode(GameMode.CREATIVE);
-                        p1.sendMessage(p.getDisplayName() + " set your gamemode Creative Mode");
-                        p.sendMessage("Set" + p1.getDisplayName() + "'s gamemode to Creative Mode");
-                    }
-                    else if(args[0].equalsIgnoreCase("2")) {
-                        p1.setGameMode(GameMode.ADVENTURE);
-                        p1.sendMessage(p.getDisplayName() + " set your gamemode to Adventure Mode");
-                        p.sendMessage("Set" + p1.getDisplayName() + "'s gamemode to Adventure Mode");
-                    }
-                    else if(args[0].equalsIgnoreCase("3")) {
-                        p1.setGameMode(GameMode.SPECTATOR);
-                        p1.sendMessage(p.getDisplayName() + " set your gamemode to Spectator Mode");
-                        p.sendMessage("Set" + p1.getDisplayName() + "'s gamemode to Spectator Mode");
-                    }
-                    return true;
-
-                }
-                return true;
-            }
         return false;
     }
 
-public void registerEvents() {
-        new JoinLeaveKickEvent(this);
-        new ChatFarben(this);
+
+        public void registerCommands() {
+            getCommand("gm").setExecutor(new GmCommand(this));
+        }
+        public void registerEvents() {
+            new JoinLeaveKickEvent(this);
+            new ChatFarben(this);
+
+        }
     }
-}
+
+
 
 
 
