5fa55ffbdec832f78cd67eb5fac11e1132c6c0e2
Meyssam120 <60116852+meyssam120@users.noreply.github.com>
2020-05-28 11:56:39 +0200

    StartInv ausgelagert (übersichtlicher) - Zeig ich dir später was ich genau gemacht hab

--- src/wasdenn/Listeners/JoinLeaveKickListener.java
+++ src/wasdenn/Listeners/JoinLeaveKickListener.java
@@ -11,34 +11,26 @@ import wasdenn.Main;
 import wasdenn.Utils.Utils;
 
 public class JoinLeaveKickListener implements Listener {
+
     private final Main plugin;
     public JoinLeaveKickListener(Main main) {
         this.plugin = main;
     }
+
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
         if (p.isOp()) {
             e.setJoinMessage("§4" + p.getName() + " §bist dem Spiel beigetreten");
         } else {
-
-
-            p.getInventory().clear();
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    p.getInventory().setItem(4, Utils.kompass());
-                    p.updateInventory();
-                }
-            }.runTaskLater(plugin, 10);
-
-            p.teleport(plugin.fm.getLocation("lobby"));
+            initialize(p); //NEU erklär ich dir später
             if(plugin.isWartung) {
                 p.kickPlayer("§cDer Server befindet sich im Wartungsmodus! Warte bis die Arbeiten abgeschlossen sind.");
             }
             e.setJoinMessage("§b" + p.getName() + " §3ist schwul");
         }
     }
+
     @EventHandler
     public void onQuit(PlayerQuitEvent e) {
         Player p = e.getPlayer();
@@ -48,12 +40,13 @@ public class JoinLeaveKickListener implements Listener {
             e.setQuitMessage("§b" + p.getName() + " §3hat das Spiel verlassen");
         }
     }
+
     @EventHandler
     public void onKick(PlayerKickEvent e) {
         Player p = e.getPlayer();
         if(p.isBanned()){
             e.setReason("§3BigMac hat dich weggebannt, diggah!");
-        return;
+            return;
         }
         if(p.isOp()) {
             e.setLeaveMessage("§fdu Arschi §f:rage:");
@@ -61,6 +54,23 @@ public class JoinLeaveKickListener implements Listener {
         else {
             e.setLeaveMessage("§fLass das §4:rage:");
         }
+    }
+
+    /*
+    Eigene Funktionen
+     */
+
+    private void initialize(Player p) {
+        p.getInventory().clear();
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                p.getInventory().setItem(4, Utils.kompass());
+                p.updateInventory();
+            }
+        }.runTaskLater(plugin, 10);
+
+        p.teleport(plugin.fm.getLocation("lobby"));
     }
 
 
